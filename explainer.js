65509a959affd6a54034e39e32061d97df6d33a6
Chih-Ching Chang <ching2032@gmail.com>
2016-10-12 16:37:32 -0500

    add histogram (without stack by genre)

--- explainer.js
+++ explainer.js
@@ -4,9 +4,10 @@ var diagonal = d3.svg.diagonal()
     .target(function(d) {return {"x":d[1].y, "y":d[1].x}; })
     .projection(function(d) { return [d.y, d.x]; });
 
-var colors = ["#8dd3c7","#ffffb3","#bebada","#fb8072"]; // if we only have 4 genres.
 // might need to let user choose how many genres they have
+var colors = ["#8dd3c7","#ffffb3","#bebada","#fb8072"]; // if we only have 4 genres.
 
+// main svg canvas
 var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height"),
@@ -19,6 +20,7 @@ var genreColor = {};
 var height = 20;
 var width = 150;
 
+// helper function to sort the dataset by its p value
 function compare(a,b) {
   if (Number(a[3]) > Number(b[3]))
     return -1;
@@ -29,6 +31,7 @@ function compare(a,b) {
 
 d3.csv("00-comedies.csv", function(data) {
    dataset = data.map(function(d) { return  [d["Item"], d["genre"], d["predicate"], d["explainer1"]]; });
+   // sort data by p value
    dataset.sort(compare);
    max = Number(dataset[dataset.length-1][3]);
    min = Number(dataset[0][3]);
@@ -36,17 +39,17 @@ d3.csv("00-comedies.csv", function(data) {
    h2 = height * dataset.length;
    // 0 -- map to max
    // height * dataset.length -- map to min
-   //console.log(dataset);
+
+   // get genre-color mapping
    k = 0;
    for (var j = 0; j < dataset.length; j++) {
-	   //console.log(dataset[j][1]);
 	   if (!(dataset[j][1] in genreColor)) {
 		   genreColor[dataset[j][1]] = colors[k];
-		   //console.log(dataset[j][1]);
 		   k++;
 	   }
    }
-   // sort data by p1 and p2
+   
+   // plot squares for each item
    for(var j = 0; j < dataset.length; j++) {
 	   svg.append("rect")
 	   .attr("height",height)
@@ -71,8 +74,7 @@ d3.csv("00-comedies.csv", function(data) {
 			 .attr("stroke","black")
 			 .attr("stroke-width","1px")
 	   })
-	   //var t = wrap(dataset[j][0],width);
-	   //console.log(t);
+	   
 	   svg.append("text")
 		  .attr("x",10 + 5)
 		  .attr("y",15+j*height)
@@ -90,25 +92,22 @@ d3.csv("00-comedies.csv", function(data) {
 		   .attr("stroke", genreColor[dataset[j][1]])
 		   .attr("fill","none")
    }
-   /*var y = d3.scale.ordinal()
-    .rangeRoundBands([height, 0], .1); // y becomes ordinal
+   
+   // draw histogram
+   histData = data.map(function(i){ return i.explainer1; });
+   var histogram = d3.layout.histogram()
+					 .bins(7)(histData)
 
-	var x = d3.scale.linear()
-		.rangeRound([0, width]); // x becomes linear
-
-	// change state group to be positioned in the y now instead of x
-	var state = svg.selectAll(".state")
-		  .data(data)
-		  .enter().append("g")
-		  .attr("class", "g")
-		  .attr("transform", function(d) { return "translate(0," + y(d.State) + ")"; });
-
-	// rect calculations become
-	 state.selectAll("rect")
-		.data(function(d) { return d.ages; })
-		.enter().append("rect")
-		.attr("height", y.rangeBand()) // height in now the rangeband
-		.attr("x", function(d) { return x(d.y0); }) // this is the horizontal position in the stack
-		.attr("width", function(d) { return x(d.y1) - x(d.y0); }) // this is the horizontal "height" of the bar
-		.style("fill", function(d) { return color(d.name); });*/
+   var box_h = h2 / 7; // num of bins, might need to change it to user input
+   var bars = svg.selectAll(".bar")
+				 .data(histogram)
+				 .enter()
+				 .append("g")
+	bars.append("rect")
+		.attr("x", function(d){ return 300; }) // fixed position for now
+		.attr("y", function(d,i){ return i*box_h; })
+		.attr("width", function(d){ return d.y*10 })
+		.attr("height", box_h)
+		.attr("fill", "steelblue")
+    
 });
