72f0c5849aece2ef058dfaa19ac675d6964c366d
Chih-Ching Chang <ching2032@gmail.com>
2016-10-20 10:25:23 -0500

    generalize

--- explainer.js
+++ explainer.js
@@ -29,73 +29,8 @@ function compare(a,b) {
   return 0;
 }
 
-function boxplot(svg, color, info, x_pos, h1, h2, max, min) {
-	svg.append("rect")
-	   .attr("height",Math.abs((info.Q3 - info.Q2)/ h1 * h2))
-	   .attr("width",20)
-	   .attr("fill",color)
-	   .attr("stroke","black")
-	   .attr("x",x_pos)
-	   .attr("y",h2 - ((info.Q3 - max)/ (-h1) * h2)) 
-	svg.append("rect")
-	   .attr("height",Math.abs((info.Q2 - info.Q1)/ h1 * h2))
-	   .attr("width",20)
-	   .attr("fill",color)
-	   .attr("stroke","black")
-	   .attr("x",x_pos)
-	   .attr("y",h2 - ((info.Q2 - max)/ (-h1) * h2))
-	svg.append("line")
-	   .attr("x1",x_pos)
-	   .attr("y1",h2 - ((info.min_val - max)/ (-h1) * h2))
-	   .attr("x2",x_pos + 20)
-	   .attr("y2",h2 - ((info.min_val - max)/ (-h1) * h2))
-	   .attr("stroke-width", 2)
-       .attr("stroke", "black")
-	svg.append("line")
-	   .attr("x1",x_pos)
-	   .attr("y1",h2-((info.max_val - max)/ (-h1) * h2))
-	   .attr("x2",x_pos+20)
-	   .attr("y2",h2-((info.max_val - max)/ (-h1) * h2))
-	   .attr("stroke-width", 2)
-       .attr("stroke", "black")
-	svg.append("line")
-	   .attr("x1",x_pos+10)
-	   .attr("y1",h2 - ((info.max_val - max)/ (-h1) * h2))
-	   .attr("x2",x_pos+10)
-	   .attr("y2",h2 - ((info.max_val - max)/ (-h1) * h2) + Math.abs((info.max_val - info.Q3)/ h1 * h2))
-	   .attr("stroke-width", 1)
-       .attr("stroke", "black")
-	svg.append("line")
-	   .attr("x1",x_pos+10)
-	   .attr("y1",h2 - ((info.Q1 - max)/ (-h1) * h2))
-	   .attr("x2",x_pos+10)
-	   .attr("y2",h2 - ((info.Q1 - max)/ (-h1) * h2) + Math.abs((info.Q1 - info.min_val)/ h1 * h2))
-	   .attr("stroke-width", 1)
-       .attr("stroke", "black")
-}
-
-d3.csv("00-comedies.csv", function(data) {
-   dataset = data.map(function(d) { return  [d["Item"], d["genre"], d["predicate"], d["explainer1"]]; });
-   // sort data by p value
-   dataset.sort(compare);
-   max = Number(dataset[dataset.length-1][3]);
-   min = Number(dataset[0][3]);
-   h1 = max - min;
-   h2 = height * dataset.length;
-   // 0 -- map to max
-   // height * dataset.length -- map to min
-
-   // get genre-color mapping
-   k = 0;
-   for (var j = 0; j < dataset.length; j++) {
-	   if (!(dataset[j][1] in genreColor)) {
-		   genreColor[dataset[j][1]] = colors[k];
-		   k++;
-	   }
-   }
-   
-   // plot squares for each item
-   for(var j = 0; j < dataset.length; j++) {
+function stackbox(svg, dataset) {
+	for(var j = 0; j < dataset.length; j++) {
 	   svg.append("rect")
 	   .attr("height",height)
 	   .attr("width",width)
@@ -137,8 +72,9 @@ d3.csv("00-comedies.csv", function(data) {
 		   .attr("stroke", genreColor[dataset[j][1]])
 		   .attr("fill","none")
    }
-   
-   // draw histogram
+}
+
+function histogramplot(svg, dataset) {
    var box_h = h2 / 7; // num of bins, might need to change it to user input
    var st = genreColor;
    for(var prop in st) {
@@ -186,29 +122,163 @@ d3.csv("00-comedies.csv", function(data) {
 			u += st[prop];
 			l++;
 	}
-   //console.log(st);
-   /*histData = data.map(function(i){ return i.explainer1; });
-   var histogram = d3.layout.histogram()
-					 .bins(7)(histData);
-	console.log(histogram);
-	histogram.reverse();
-   
-   var bars = svg.selectAll(".bar")
-				 .data(histogram)
-				 .enter()
-				 .append("g")
-	bars.append("rect")
-		.attr("x", function(d){ return 300; }) // fixed position for now
-		.attr("y", function(d,i){ return i*box_h; })
-		.attr("width", function(d){ return d.y*10 })
-		.attr("height", box_h)
-		.attr("fill", "steelblue")*/
+}
+
+function boxplot(svg, color, info, x_pos, h1, h2, max, min) {
+	svg.append("rect")
+	   .attr("height",Math.abs((info.Q3 - info.Q2)/ h1 * h2))
+	   .attr("width",20)
+	   .attr("fill",color)
+	   .attr("stroke","black")
+	   .attr("x",x_pos)
+	   .attr("y",h2 - ((info.Q3 - max)/ (-h1) * h2)) 
+	svg.append("rect")
+	   .attr("height",Math.abs((info.Q2 - info.Q1)/ h1 * h2))
+	   .attr("width",20)
+	   .attr("fill",color)
+	   .attr("stroke","black")
+	   .attr("x",x_pos)
+	   .attr("y",h2 - ((info.Q2 - max)/ (-h1) * h2))
+	svg.append("line")
+	   .attr("x1",x_pos)
+	   .attr("y1",h2 - ((info.min_val - max)/ (-h1) * h2))
+	   .attr("x2",x_pos + 20)
+	   .attr("y2",h2 - ((info.min_val - max)/ (-h1) * h2))
+	   .attr("stroke-width", 2)
+       .attr("stroke", "black")
+	svg.append("line")
+	   .attr("x1",x_pos)
+	   .attr("y1",h2-((info.max_val - max)/ (-h1) * h2))
+	   .attr("x2",x_pos+20)
+	   .attr("y2",h2-((info.max_val - max)/ (-h1) * h2))
+	   .attr("stroke-width", 2)
+       .attr("stroke", "black")
+	svg.append("line")
+	   .attr("x1",x_pos+10)
+	   .attr("y1",h2 - ((info.max_val - max)/ (-h1) * h2))
+	   .attr("x2",x_pos+10)
+	   .attr("y2",h2 - ((info.max_val - max)/ (-h1) * h2) + Math.abs((info.max_val - info.Q3)/ h1 * h2))
+	   .attr("stroke-width", 1)
+       .attr("stroke", "black")
+	svg.append("line")
+	   .attr("x1",x_pos+10)
+	   .attr("y1",h2 - ((info.Q1 - max)/ (-h1) * h2))
+	   .attr("x2",x_pos+10)
+	   .attr("y2",h2 - ((info.Q1 - max)/ (-h1) * h2) + Math.abs((info.Q1 - info.min_val)/ h1 * h2))
+	   .attr("stroke-width", 1)
+       .attr("stroke", "black")
+}
+
+d3.csv("00-comedies.csv", function(data) {
+   // get column name
+   var dataValues = d3.values(data)[0];
+   var explainer = [];
+   var predicate = "";
+   var item = "";
+   var genre = "";
+   // know the indices of different column we want
+   var col_names = Object.keys(dataValues);
+   for(var i = 0; i < col_names.length; i++) {
+	   var str = col_names[i].toLowerCase();
+	   if (str === "predicate") {
+		   predicate = col_names[i];
+	   } else if (str === "item") {
+		   item = col_names[i];
+	   } else if (str === "genre") {
+		   genre = col_names[i];
+	   } else {		   
+		   explainer.push(col_names[i]);
+	   }
+   }
+	/*console.log(item);
+	console.log(predicate);
+	console.log(genre);
+	console.log(explainer);*/
+	// get genre-color mapping
+   k = 0;
+   var genres = data.map(function(d) { return  d[genre]; });
+   //console.log(genres);
+   for (var j = 0; j < genres.length; j++) {
+	   if (!(genres[j] in genreColor)) {
+		   genreColor[genres[j]] = colors[k];
+		   k++;
+	   }
+   }
+   //console.log(genreColor);
+	if (predicate !== "") {
+	   for (var i = 0; i < explainer.length; i++) {
+		  dataset = data.map(function(d) { return  [d[item], d[genre], d[predicate], d[explainer[i]]]; });
+		  // sort
+		  dataset.sort(compare);
+		  max = Number(dataset[dataset.length-1][3]);
+		  min = Number(dataset[0][3]);
+		  h1 = max - min;
+		  h2 = height * dataset.length;
+		  // stack box and lines
+		  stackbox(svg, dataset);
+		  // histogram
+		  histogramplot(svg, dataset);
+		  // box
+		  var positive = [];
+			var negative = [];
+			var all_val = [];
+			for (var j = 0;j < dataset.length; j++) {
+				if (Number(dataset[j][2]) > 0) {
+					positive.push(dataset[j][3]);
+				} else {
+					negative.push(dataset[j][3]);
+				}
+				all_val.push(dataset[j][3]);
+			}
+			positive.sort(compare);
+			var p1 = positive.map(Number);
+			negative.sort(compare);
+			var n1 = negative.map(Number);
+			all_val.sort(compare);
+			var a1 = all_val.map(Number);
+			// this part is ugly...
+			var p_info = {
+				max_val: p1[0],
+				Q3: p1[Math.floor(p1.length * 1 / 4)],
+				Q2: p1[Math.floor(p1.length * 1 / 2)],
+				Q1: p1[Math.floor(p1.length * 3 / 4)],
+				min_val: p1[p1.length - 1],
+			};
+			boxplot(svg, colors[0], p_info, 500, h1, h2, max, min);
+			var n_info = {
+				max_val: n1[0],
+				Q3: n1[Math.floor(n1.length * 1 / 4)],
+				Q2: n1[Math.floor(n1.length * 1 / 2)],
+				Q1: n1[Math.floor(n1.length * 3 / 4)],
+				min_val: n1[n1.length -1],
+			};
+			boxplot(svg, colors[2], n_info, 470, h1, h2, max, min);
+			var a_info = {
+				max_val: a1[0],
+				Q3: a1[Math.floor(a1.length * 1 / 4)],
+				Q2: a1[Math.floor(a1.length * 1 / 2)],
+				Q1: a1[Math.floor(a1.length * 3 / 4)],
+				min_val: a1[a1.length -1],
+			};
+			boxplot(svg, colors[1], a_info, 440, h1, h2, max, min);
+		  dataset = [];
+	   }
+	} else {
+	   for (var i = 0; i < explainer.length; i++) {
+		  dataset = data.map(function(d) { return  [d[item], d[genre], d[explainer[i]]]; });
+		  // sort
+		  // line
+		  // histogram
+		  // box without pos and neg
+	   }
+	}
 		
 	// draw box plot, +1 / -1 / all
-	var positive = [];
+	// only draw positive and negative ones if we have predicate
+	/*var positive = [];
 	var negative = [];
 	var all_val = [];
-	for (var j = 0;j<dataset.length; j++) {
+	for (var j = 0;j < dataset.length; j++) {
 		if (Number(dataset[j][2]) > 0) {
 			positive.push(dataset[j][3]);
 		} else {
@@ -246,6 +316,6 @@ d3.csv("00-comedies.csv", function(data) {
 		Q1: a1[Math.floor(a1.length * 3 / 4)],
 		min_val: a1[a1.length -1],
 	};
-	boxplot(svg, colors[1], a_info, 440, h1, h2, max, min);
+	boxplot(svg, colors[1], a_info, 440, h1, h2, max, min);*/
 
 });
